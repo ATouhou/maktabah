0e4344a5a77f573a87a1e5a44f481b3e9dc9a434
bagol <m.awad.syahid@gmail.com>
2016-05-30 00:46:35 +0700

    refactor

--- README.md
+++ README.md
@@ -2,6 +2,64 @@
 
 Maktabah shamela utility
 
+# usage
+
+```
+  Usage: maktabah [options] [command]
+
+
+  Commands:
+
+    dump <book>   dump maktabah bookfile to json
+    schema        install or uninstall maktabah schema
+    kitab <book>  install or uninstall maktabah kitab
+    help [cmd]    display help for [cmd]
+
+  Maktabah shamela utility
+
+  Options:
+
+    -h, --help     output usage information
+    -V, --version  output the version number
+```
+
+```
+  Usage: maktabah-dump [options]
+
+  dump maktabah bookfile to json
+
+  Options:
+
+    -h, --help            output usage information
+    -s, --spaces <count>  JSON space count
+```
+
+```
+  Usage: maktabah-schema [options]
+
+  install or uninstall maktabah schema
+
+  Options:
+
+    -h, --help                  output usage information
+    -u, --uninstall             uninstall schema instead of install
+    -c, --config <config-file>  path to knex configurations file
+    -d, --dump                  dump query instead of excuting directly
+```
+
+```
+  Usage: maktabah-kitab [options]
+
+  install or uninstall maktabah kitab
+
+  Options:
+
+    -h, --help                  output usage information
+    -u, --uninstall             uninstall kitab instead of install
+    -c, --config <config-file>  path to knex configurations file
+    -d, --dump                  dump query instead of excuting directly
+```
+
 # requirements
 
 [mdb-tools](https://github.com/brianb/mdbtools) must be installed.
--- test/spec.js
+++ test/spec.js
@@ -1,4 +1,5 @@
 const chai     = require('chai');
+const Promise  = require('bluebird');
 const maktabah = require('..');
 
 chai.use(require('chai-things'));
@@ -6,64 +7,54 @@ chai.Should();
 
 describe('maktabah', () => {
 
+  const config = {
+    client: 'sqlite3',
+    connection: {
+      filename: ':memory:'
+    },
+    pool: {
+      afterCreate(conn, cb) {
+        // ensure foreign key constraint
+        conn.run('PRAGMA foreign_keys = ON', cb);
+      }
+    },
+    useNullAsDefault: true  // suppress warning
+  };
+
   describe('schema', () => {
 
-    it('dumps schema', () => {
-      const result = maktabah.schema();
-      result.should.be.a('string');
+    const knex = require('knex')(config);
+
+    it('install schema', () => {
+      return Promise.reduce(maktabah.installSchema(knex), p => p)
+        .then(() => knex('sqlite_master').select('name'))
+        .then((tables) => {
+          tables.map(table => table.name).should.deep.equal([
+            'master', 'titles', 'nash', 'sPdf', 'Shrooh', 'Shorts', 'oShrooh',
+            'oShr', 'nBound', 'men_u', 'men_h', 'men_b', 'com', 'avPdf', 'abc'
+          ]);
+        })
     });
 
-    it('creates schema', () => {
-      const config = {
-        client: 'sqlite3',
-        connection: {
-          filename: ':memory:'
-        },
-        useNullAsDefault: true
-      };
-      return maktabah.schema({ config, dump: false })
-        .then((knex) => knex('sqlite_master').select('name'))
-        .then((res) => {
-          res.should.contain.an.item.with.property('name', 'category');
-          res.should.contain.an.item.with.property('name', 'kitab');
-          res.should.contain.an.item.with.property('name', 'author');
-          res.should.contain.an.item.with.property('name', 'kitab_author');
-          res.should.contain.an.item.with.property('name', 'nash');
-          res.should.contain.an.item.with.property('name', 'part');
-        });
-    });
+    it('drop schema');
 
   });
 
-  describe('installation', () => {
+  describe('kitab', () => {
 
-    const book = __dirname + '/data/jurumiyyah.bok';
+    const knex = require('knex')(config);
 
-    it('dumps kitab installation', () => {
-      return maktabah.install(book)
-        .then((result) => {
-          result.should.be.a('string');
+    it('install kitab', () => {
+      return Promise.reduce(maktabah.installSchema(knex), p => p)
+        .then(() => Promise.reduce(maktabah.installKitab(knex, require('./data/jurumiyyah')), p => p))
+        .then(() => knex('master').select().first())
+        .then((master) => {
+          master.BkId.should.equal(11371);
+          master.Bk.should.equal('الآجرومية');
         });
     });
 
-    it('installs kitab', () => {
-      const config = {
-        client: 'sqlite3',
-        connection: {
-          filename: ':memory:'
-        },
-        useNullAsDefault: true
-      };
-
-      const knex = require('knex')(config);
-
-      return maktabah.schema({ knex, dump: false })
-        .then(() => maktabah.install(book, { knex, dump: false, category: true }))
-        .then(() => knex('kitab').first())
-        .then((kitab) => {
-          kitab.name.should.equal('الآجرومية');
-        })
-    });
+    it('drop kitab');
 
   });
 
